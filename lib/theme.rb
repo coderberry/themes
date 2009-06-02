0af3cb1f57da72c6a9c10544edad7a0eb098cf84
Eric Berry <cavneb@gmail.com>
2009-06-01 22:24:21 -0600

    added a base css to override blueprint css

--- lib/theme.rb
+++ lib/theme.rb
@@ -2,7 +2,7 @@ module Theme
   module ThemeHelper
     include ActionView::Helpers::AssetTagHelper
     def use_theme(theme_name)
-      stylesheet_link_tag "/themes/#{theme_name}/css/style.css"
+      stylesheet_link_tag "/themes/base.css", "/themes/#{theme_name}/css/style.css"
     end
   end
 end
