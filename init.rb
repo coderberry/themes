9c6e2927ff98e85eb17156e713e9894a4998bac2
Eric Berry <cavneb@gmail.com>
2009-11-03 20:45:08 -0700

    made some minor adjustments and fixed the site config yaml loader

--- init.rb
+++ init.rb
@@ -1,2 +1,4 @@
-require 'theme'
+require File.join(File.dirname(__FILE__), "lib", "theme")
+require File.join(File.dirname(__FILE__), "lib", "flash_helper")
 ActionView::Base.send(:include, Theme::ThemeHelper)
+ActionView::Base.send(:include, FlashHelper)
